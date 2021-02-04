26efccf15e085c85c3682df94b5eef86a99c7edc
cristianebavila <cristianebavila@gmail.com>
2021-02-04 14:55:16 -0300

    quadrado funny

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
+<h1>Bem vindo ao Treinamento da Gama Turma 14 Itaú </h1>
 
 <h3> Um deploy automático feito com alunos do Itaú </h3>
 
-<div style="width:200px;height:200px;background:yellow">
+<div style="display:flex">
+<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px"> 
     Um teste em um quadrado
+</div>
+
+<div style="width:200px;height:200px;background:slateblue;color:white;padding:50px;margin:10px">
+    Um teste em um quadrado
+</div>
 </div>
