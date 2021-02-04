03fc4046246690e939d670b7992f8fc7c77b15a2
cristianebavila <cristianebavila@gmail.com>
2021-02-04 14:21:35 -0300

    quadrado

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
+<h3> Um deploy automático feito com alunos do Itaú </h3>
+
+<div style="width:200px;height:200px;backgroup:yellow">
+    Um teste em um quadrado
+</div>
