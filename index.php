efe6f8dd66ecde59c27f5c437dfed3e6713c3acb
cristianebavila <cristianebavila@gmail.com>
2021-02-04 15:41:03 -0300

    links de compra

--- index.php
+++ index.php
@@ -3,15 +3,28 @@
 <h3> Um deploy automático feito com alunos do Itaú </h3>
 
 <div style="display:flex">
-<div style="width:200px;height:200px;background:lightgreen;padding:50px;margin:10px"> 
+<div style="width:200px;height:400px;background:lightgreen;padding:50px;margin:10px"> 
 <i><b><center>    Um teste em um quadrado </center></b></i>
 <br>
 <center><img src="bike1.jpeg" style="width:100px"> </center>
+<br></br>
+R$550,00
+<br><br>
+<a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
 </div>
 
-<div style="width:200px;height:200px;background:dodgerblue;color:white;padding:50px;margin:10px">
+<div style="width:200px;height:400px;background:dodgerblue;color:white;padding:50px;margin:10px">
 <i><b><center>    Um teste em um quadrado </center></b></i>
 <br>
 <center><img src="bike2.jpeg" style="width:100px"> </center>
+<br></br>
+R$600,00
+<br>
+<br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+  </div>
+
 </div>
 </div
